c5649b489ea6bf23ff0133d308bbe0b8c809c9a2
chris <morales.cristhian@proton.me>
2026-09-18 11:58:21 -0700

    ya funciona

--- src/main/java/com/example/practica2/vista/VistaJuego.java
+++ src/main/java/com/example/practica2/vista/VistaJuego.java
@@ -141,7 +141,7 @@ public class VistaJuego {
             Button deshacer = new Button("DESHACER MOVIMIENTO");
             deshacer.setStyle(estiloBoton);
             tomarCarta.setStyle(estiloBoton);
-            // AUN NO FUNCIONA
+            // YA FUNCIONA
             parar.setStyle(estiloBoton);
 
             tomarCarta.setOnAction(e -> controlador.pedirCarta());
